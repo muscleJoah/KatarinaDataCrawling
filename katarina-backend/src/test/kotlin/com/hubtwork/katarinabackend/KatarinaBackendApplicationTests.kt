b5880ec12e839e251c1b3bff855f4c4f8f21ff71
Hubtwork <hubtwork@gmail.com>
2021-01-13 10:38:27 +0900

    Init Project

---
+++ katarina-backend/src/test/kotlin/com/hubtwork/katarinabackend/KatarinaBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hubtwork.katarinabackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KatarinaBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
