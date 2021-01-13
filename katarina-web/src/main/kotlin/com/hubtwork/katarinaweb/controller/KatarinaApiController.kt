8da8c1752a212d5a032d26c9882cfdb961a71274
Hubtwork <hubtwork@gmail.com>
2021-01-13 11:00:47 +0900

    main: Init

---
+++ katarina-web/src/main/kotlin/com/hubtwork/katarinaweb/controller/KatarinaApiController.kt
@@ -0,0 +1,16 @@
+package com.hubtwork.katarinaweb.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/katarina")
+class KatarinaApiController {
+
+    @GetMapping("")
+    fun controllerRootDir() =
+        " API 메인 페이지 "
+
+
+}
