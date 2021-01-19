14d572abb025adc1d8db729c5720662c7e25ac2b
Hubtwork <hubtwork@gmail.com>
2021-01-19 17:36:56 +0900

    master: Init api server ADD Api server communicate with riot api

---
+++ katarina-api/src/main/kotlin/com/hubtwork/katarinaapi/KatarinaApiApplication.kt
@@ -0,0 +1,11 @@
+package com.hubtwork.katarinaapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KatarinaApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<KatarinaApiApplication>(*args)
+}
---
+++ katarina-api/src/test/kotlin/com/hubtwork/katarinaapi/KatarinaApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hubtwork.katarinaapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KatarinaApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
